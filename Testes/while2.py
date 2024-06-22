22b3c019e594cbde213acc5d193c5ea627194e13
Andreza Taina <andrezatainaaa@gmail.com>
2024-06-22 17:09:54 -0300

    Semana04_atividades_aula

---
+++ Testes/while2.py
@@ -0,0 +1,6 @@
+balas = 0
+
+while balas < 5:
+    print('Eu tenho', balas, 'balas')
+    balas = balas + 1
+    
---
+++ Testes/while3.py
@@ -0,0 +1,7 @@
+numero = int(input('Digite um número: '))
+
+while numero >= 0:
+    resultado = numero * 2
+    print(f'O resultado do {numero} multiplicado por 2 é {resultado}')
+
+print('Feito')
